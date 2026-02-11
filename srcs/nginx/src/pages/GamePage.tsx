8bc070d11a5da5f7a38e646c67635b9bf27ca69b
ilia <ilia@ilias-MacBook-Pro.local>
2026-02-11 10:15:16 +0100

    feat: Added GamePage an relative function ton game UI

---
+++ srcs/nginx/src/pages/GamePage.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from '../components/molecules/NavBar';
+
+export const GamePage = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <div className="text-center">GAME PLAY</div>
+    </div>
+  );
+};
